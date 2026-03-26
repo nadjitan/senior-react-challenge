197b960ca80f38da27c845bc5e9d06d61272613a
nadjitan <nadjitan@yahoo.com.ph>
2026-03-26 18:30:49 +0800

    chore: initial project skeleton (no implementation)

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center">Home</div>
+  );
+}
---
+++ src/app/users/Presentational.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const Presentational: React.FC = () => {
+  return <div></div>;
+};
