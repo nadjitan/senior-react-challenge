db19ead44d8fa258eb527307879c5cf1e42941a0
nadjitan <nadjitan@yahoo.com.ph>
2026-03-26 18:42:10 +0800

    feat: created User types

---
+++ src/types/user.ts
@@ -0,0 +1,72 @@
+export type GetUsersResponse = {
+  limit: number;
+  skip: number;
+  total: number;
+  users: User[];
+};
+
+export type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  maidenName: string;
+  age: number;
+  gender: "male" | "female";
+  email: string;
+  phone: string;
+  username: string;
+  password: string;
+  birthDate: string;
+  image: string;
+  bloodGroup: string;
+  height: number;
+  weight: number;
+  eyeColor: string;
+  hair: {
+    color: string;
+    type: string;
+  };
+  ip: string;
+  address: Address;
+  macAddress: string;
+  university: string;
+  bank: {
+    cardExpire: string;
+    cardNumber: string;
+    cardType: string;
+    currency: string;
+    iban: string;
+  };
+  company: {
+    department: string;
+    name: string;
+    title: string;
+    address: Address;
+  };
+  ein: string;
+  ssn: string;
+  userAgent: string;
+  crypto: {
+    coin: string;
+    wallet: string;
+    network: string;
+  };
+  role: "admin" | "moderator" | "user";
+};
+
+type Address = {
+  address: string;
+  city: string;
+  state: string;
+  stateCode: string;
+  postalCode: string;
+  coordinates: {
+    lat: number;
+    lng: number;
+  };
+  country: string;
+};
+
+export type UsersFilter = {
+  gender: "male" | "female" | "all";
+};
